44665e0319e37047da29d5decf6095fcc6f0c737
ZPollar0 <zpollard2000@gmail.com>
2024-04-09 16:00:49 -0400

    basics of java

---
+++ workbook-1/First-Maven-Project/src/main/java/com/pluralsight/PracticeOperations.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class PracticeOperations {
+
+    public static void main(String[] args) {
+        String word1 = "Love";
+        String word2 = "to";
+        String word3 = "code";
+        String Combination;
+        Combination = word1 + " " + word2 + " " + word3;
+        System.out.println(Combination);
+    }
+}
---
+++ workbook-1/First-Maven-Project/src/main/java/com/pluralsight/PriceApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class PriceApp {
+
+    public static void main(String args[]){
+        int count = 11;
+        float unitPrice = 7.12f; //this is an error without the f
+        float taxRate = 0.825f;
+        float totalCost;
+        totalCost = (count * unitPrice) * (1 + taxRate);
+        System.out.println(totalCost);
+    }
+}
