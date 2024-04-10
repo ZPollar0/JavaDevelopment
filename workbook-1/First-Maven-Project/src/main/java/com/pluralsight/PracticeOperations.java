6a6648a4a10dc5513b3054923f67fe7525465355
ZPollar0 <zpollard2000@gmail.com>
2024-04-10 11:31:10 -0400

    completed PracticeOperations

--- workbook-1/First-Maven-Project/src/main/java/com/pluralsight/PracticeOperations.java
+++ workbook-1/First-Maven-Project/src/main/java/com/pluralsight/PracticeOperations.java
@@ -3,10 +3,11 @@ package com.pluralsight;
 public class PracticeOperations {
 
     public static void main(String[] args) {
-        String word1 = "Love";
-        String word2 = "to";
-        String word3 = "code";
+        String word1 = "Love ";
+        String word2 = "to ";
+        String word3 = "code!";
         String Combination;
+        System.out.println(word1 + word2 + word3);
         Combination = word1 + " " + word2 + " " + word3;
         double numba = 18.31;
         double numba2 = 6.01;
